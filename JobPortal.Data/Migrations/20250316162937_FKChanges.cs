179883007d292c7e3e267a658d83bb1e13fa8312
KARTHIK S <karthikshankar1101@gmail.com>
2025-03-16 22:02:35 +0530

    Model Naming changes for FK

---
+++ JobPortal.Data/Migrations/20250316162937_FKChanges.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobPortal.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FKChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
